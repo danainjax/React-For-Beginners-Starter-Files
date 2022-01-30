c05622de530e033d28eb70a57394bcc5e49cce1d
danainjax <danainjax@gmail.com>
2022-01-29 20:32:18 -0500

    added StorePicker component

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,2 +1,5 @@
-// let's go!
-//comment testing
+import React from 'react'
+import { render } from 'react-dom'
+import StorePicker from './components/StorePicker'
+
+render(<StorePicker />, document.getElementById('main'))
